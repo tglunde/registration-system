76532e5d8686f6edd4a86abedd937ee53535978e
ilikutle <ilijakutle@gmail.com>
2020-01-15 19:33:55 +0100

    new data model

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 from flask import Flask,render_template,flash, redirect,url_for,session,logging,request
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class contestant(db.Model):
+
+class Contestant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    gender = db.Column(db.String(10))
     first_name = db.Column(db.String(20))
     last_name = db.Column(db.String(80))
     email = db.Column(db.String(80), unique=True)
     year_of_birth = db.Column(db.Integer)
-    nationality = db.Column(db.String(30))
     telephone = db.Column(db.String(25))
-    zip_code = db.Column(db.String(15))
-    street = db.Column(db.String(100))
-    city = db.Column(db.String(25))
-    country = db.Column(db.String(25))
-    gender = db.Column(db.String(10))
     club = db.Column(db.String(50))
-    contest = db.Column(db.String(20))
-
+    contest = db.Column(db.String(25))
+    confirmation = db.Column(db.String())
+    time = db.Column(db.DateTime(), default=datetime.datetime.now())
+    active = db.Column(db.Boolean(), default=False)
+    ip = db.Column(db.String(16))
 
 @app.route('/')
 def index():
@@ -29,12 +29,13 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+
+
     if request.method == 'POST':
-        new_contestant = contestant(first_name = request.form['firstname'], last_name = request.form['lastname'], 
-        email = request.form['email'], street = request.form['street'], telephone = request.form['telephone'],
-        year_of_birth = request.form['yob'], nationality = request.form['nationality'], 
-        zip_code = request.form['zipcode'], city = request.form['city'], country = request.form['country'],
-        gender = request.form['gender'],club = request.form['club'], contest = request.form['contest'])
+        new_contestant = Contestant(first_name = request.form['firstname'], last_name = request.form['lastname'], 
+        email = request.form['email'], telephone = request.form['telephone'],
+        year_of_birth = request.form['yob'], contest = request.form['contest'],
+        gender = request.form['gender'],club = request.form['club'], ip = request.environ['REMOTE_ADDR'])
 
         db.session.add(new_contestant)
         db.session.commit()
